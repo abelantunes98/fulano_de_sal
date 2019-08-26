024cc2e8795c435deefff6f7adccddcb62be2db3
samuelcomputacao <samuel25101998@gmail.com>
2019-08-26 05:00:24 -0300

    .

---
+++ src/main/java/br/com/marmitaria/MarmitariaApplication.java
@@ -0,0 +1,12 @@
+package br.com.marmitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmitariaApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/marmitaria/MarmitariaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.marmitaria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarmitariaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
