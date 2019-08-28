0dfa527d9b8742a2172eea6abd9ee21f1bb65aa1
samuelcomputacao <samuel25101998@gmail.com>
2019-08-28 20:08:41 -0300

    .

--- src/main/java/br/com/marmitaria/MarmitariaApplication.java
+++ src/main/java/br/com/marmitaria/MarmitariaApplication.java
@@ -1,13 +1,25 @@
 package br.com.marmitaria;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MarmitariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarmitariaApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String despert() {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return format.format(new Date());
 	}
 
 }
--- src/main/java/br/com/marmitaria/rest/controller/UsuarioController.java
+++ src/main/java/br/com/marmitaria/rest/controller/UsuarioController.java
@@ -1,16 +1,12 @@
 package br.com.marmitaria.rest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
-	
-	@GetMapping("/hello")
-	public String helloWold() {
-		return "helloWold!";
-	}
+
 
 }
