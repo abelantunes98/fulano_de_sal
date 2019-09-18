686bc7733f5089d10743eb97be5abd00928d2e40
samuelcomputacao <samuel25101998@gmail.com>
2019-09-18 14:37:01 -0400

    .

--- src/main/java/br/com/marmitaria/rest/util/Validation.java
+++ src/main/java/br/com/marmitaria/rest/util/Validation.java
@@ -1,5 +1,8 @@
 package br.com.marmitaria.rest.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import br.com.marmitaria.rest.exception.DadosInvalidosException;
 import br.com.marmitaria.rest.request.ClienteRequest;
 import br.com.marmitaria.rest.request.LoginRequest;
@@ -7,10 +10,21 @@ import br.com.marmitaria.rest.request.UsuarioRequest;
 
 public abstract class Validation {
 	
+	private static final String EMAIL_PATTERN = 
+	        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+	        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+	
 	public static void validaLogin(LoginRequest login) {
 		
 		if(naoInformado(login.getEmail())) {
 			throw new DadosInvalidosException("Email não informado!");
+		}
+		Matcher matcher = pattern.matcher(login.getEmail());
+		
+		if(matcher.matches()) {
+			throw new DadosInvalidosException("Email inválido!");
 		}
 		
 		if(naoInformado(login.getSenha())) {
@@ -34,6 +48,12 @@ public abstract class Validation {
 		if(naoInformado(request.getEmail())) {
 			throw new DadosInvalidosException("Email não informado!");
 		}
+		Matcher matcher = pattern.matcher(request.getEmail());
+		
+		if(matcher.matches()) {
+			throw new DadosInvalidosException("Email inválido!");
+		}
+		
 		if(naoInformado(request.getSenha())) {
 			throw new DadosInvalidosException("Senha não informada!");
 		}
